9c084ee30dee6c08d7d0d23181ea5f5bcb8556da
Jose Hilario Almeida <josealmeida@gmail.com>
2016-05-05 12:18:35 +0100

    added ex3 project scaffolding files

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 ## Pro ASP.NET MVC 4
 
-- ex1 - page 16
-- ex2 - page 27
+- ex1 - page 16 - adding controler and view
+- ex2 - page 27 - data entry and model
+- ex3 - page 38 - validation
---
+++ ex3/ex3/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ex3.Models;
+
+namespace ex3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            // TODO: Email response to the party organizer
+            return View("Thanks", guestResponse);
+        }
+    }
+}
