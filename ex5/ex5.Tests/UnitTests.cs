9d714eab66b3cf8e1ac34d7358db20e27726487f
Jose Hilario Almeida <josealmeida@gmail.com>
2016-05-06 09:43:49 +0100

    added Product model and its test

---
+++ ex5/ex5.Tests/UnitTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ex5.Models;
+
+namespace ex5.Tests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod()]
+        public void addProduct() {
+            Product testProduct = new Product {
+                ProductID = 100,
+                Name = "Raft",
+                Description = "A boat for one person",
+                Price = 275M,
+                Category = "Watersports"
+            };
+
+            Assert.AreEqual(testProduct.ProductID, 100);
+            Assert.AreEqual(testProduct.Name, "Raft");
+            Assert.AreEqual(testProduct.Description, "A boat for one person");
+            Assert.AreEqual(testProduct.Price, 275M);
+            Assert.AreEqual(testProduct.Category, "Watersports");
+        }
+    }
+}
--- ex5/ex5/Controllers/HomeController.cs
+++ ex5/ex5/Controllers/HomeController.cs
@@ -28,5 +28,32 @@ namespace ex5.Controllers
             return View("Result",
             (object)String.Format("Product name: {0}", productName));
         }
+
+        public ViewResult CreateProduct()
+        {
+            // create and populate a new Product object
+            Product myProduct = new Product {
+                ProductID = 100,
+                Name = "Raft",
+                Description = "A boat for one person",
+                Price = 275M,
+                Category = "Watersports"
+            };
+
+            return View("Result",
+            (object)String.Format("Category: {0}", myProduct.Category));
+        }
+
+        public ViewResult CreateCollection()
+        {
+            string[] stringArray = { "apple", "orange", "plum" };
+
+            List<int> intList = new List<int> { 10, 20, 30, 40 };
+
+            Dictionary<string, int> myDict = new Dictionary<string, int> {
+                { "apple", 10 }, { "orange", 20 }, { "plum", 30 }};
+
+            return View("Result", (object)stringArray[1]);
+        }
     }
 }
