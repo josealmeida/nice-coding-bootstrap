44ae78a8d1da6391c40bb72c65fc4dfcece3aaf8
Jose Hilario Almeida <josealmeida@gmail.com>
2016-05-09 09:49:13 +0100

    added IEnumerable to ShoppingCart and ShoppingCartExtention

--- ex5/ex5/Models/ShoppingCart.cs
+++ ex5/ex5/Models/ShoppingCart.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace ex5.Models
 {
-    public class ShoppingCart
+    public class ShoppingCart: IEnumerable<Product>
     {
         public List<Product> Products { get; set; }
+
+        public IEnumerator<Product> GetEnumerator()
+        {
+            return Products.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
--- ex5/ex5/Models/ShoppingCartExtention.cs
+++ ex5/ex5/Models/ShoppingCartExtention.cs
@@ -7,9 +7,9 @@ namespace ex5.Models
 {
     public static class ShoppingCartExtention
     {
-        public static decimal TotalPrices(this ShoppingCart cartParam) {
+        public static decimal TotalPrices(this IEnumerable<Product> productEnum) {
             decimal total = 0;
-            foreach (Product prod in cartParam.Products) {
+            foreach (Product prod in productEnum) {
                 total += prod.Price;
             }
             return total;
