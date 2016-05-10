389b6f3c4a5d46ce395dcbe7e7b765d6429bc573
Jose Hilario Almeida <josealmeida@gmail.com>
2016-05-10 14:22:47 +0100

    added HttpAsync class

---
+++ ex5/ex5/Models/HttpAsync.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace ex5.Models
+{
+    public class HttpAsync
+    {
+        public static Task<long?> GetPageLength(){
+
+            HttpClient client = new HttpClient();
+            var httpTask = client.GetAsync("http://google.com");
+
+            return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) => {
+                return antecedent.Result.Content.Headers.ContentLength;
+            });
+        }
+    }
+}
