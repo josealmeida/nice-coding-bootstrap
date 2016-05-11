268e84323feed4fbee5c038bfe63e0a2a6a9546b
Jose Hilario Almeida <josealmeida@gmail.com>
2016-05-11 10:25:39 +0100

    added DemoExpression view

--- csharp/ex6/ex6/Controllers/HomeController.cs
+++ csharp/ex6/ex6/Controllers/HomeController.cs
@@ -22,5 +22,13 @@ namespace ex6.Controllers
         public ActionResult NameAndPrice() {
             return View(myProduct);
         }
+
+        public ActionResult DemoExpression() {
+            ViewBag.ProductCount = 1;
+            ViewBag.ExpressShip = true;
+            ViewBag.ApplyDiscount = false;
+            ViewBag.Supplier = null;
+            return View(myProduct);
+        }
     }
 }
