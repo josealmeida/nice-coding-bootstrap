d146f54b94fa7b9503709f6d3dfcd829969e701b
Jose Hilario Almeida <josealmeida@gmail.com>
2016-05-12 13:31:09 +0100

    added tutorial scripts to new project

---
+++ fsharp/why-use-fsharp-tutorial/src/WhyUseFsharpTutorial/exercises/03SumOfSquares.fsx
@@ -0,0 +1,16 @@
+﻿// from https://fsharpforfunandprofit.com/posts/fvsc-sum-of-squares/
+
+// define the square function
+let square x = x * x
+// define the sumOfSquares function
+let sumOfSquares n = 
+   [1..n] |> List.map square |> List.sum
+// try it
+sumOfSquares 100 |> printfn "%i"
+
+// define the square function
+let squareF x = x * x
+// define the sumOfSquares function
+let sumOfSquaresF n = 
+   [1.0 .. n] |> List.map squareF |> List.sum  // "1.0" is a float
+sumOfSquaresF 100.0 |> printfn "%f"
