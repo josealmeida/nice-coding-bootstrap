9b177cb35c5d5cb0c17506b917fa9ab06e59f060
Jose Hilario Almeida <josealmeida@gmail.com>
2016-05-13 12:10:21 +0100

    added Models - Product, LinqValueCalculator, ShoppingCart

---
+++ csharp/ex7/ex7/Models/LinqValueCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ex7.Models
+{
+    public class LinqValueCalculator
+    {
+        public decimal ValueProducts(IEnumerable<Product> products) {
+            return products.Sum(p => p.Price);
+        }
+    }
+}
