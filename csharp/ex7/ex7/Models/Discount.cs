45a0cc80752e6f9257688cf21dd0e73c43d3d272
Jose Hilario Almeida <josealmeida@gmail.com>
2016-05-13 14:11:57 +0100

    added Discount model

---
+++ csharp/ex7/ex7/Models/Discount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ex7.Models
+{
+    public class Discount
+    {
+        public interface IDiscountHelper
+        {
+            decimal ApplyDiscount(decimal totalParam);
+        }
+
+        public class DefaultDiscountHelper : IDiscountHelper
+        {
+            public decimal ApplyDiscount(decimal totalParam)
+            {
+                return (totalParam - (10m / 100m * totalParam));
+            }
+        }
+    }
+}
