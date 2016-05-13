a9ea211b4b56a7c276a2c70eeb9d639f36c101d0
Jose Hilario Almeida <josealmeida@gmail.com>
2016-05-13 14:07:09 +0100

    added NinjectDependencyResolver and changed project to use it

--- csharp/ex7/ex7/Controllers/HomeController.cs
+++ csharp/ex7/ex7/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ex7.Models;
+using Ninject;
 
 namespace ex7.Controllers
 {
@@ -15,9 +16,13 @@ namespace ex7.Controllers
             new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
             new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
         };
+        private IValueCalculator calc;
+
+        public HomeController(IValueCalculator calcParam) {
+            calc = calcParam;
+        }
 
         public ActionResult Index() {
-            IValueCalculator calc = new LinqValueCalculator();
             ShoppingCart cart = new ShoppingCart(calc) { Products = products };
             decimal totalValue = cart.CalculateProductTotal();
             return View(totalValue);
---
+++ csharp/ex7/ex7/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,30 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using ex7.Models;
+using System.Web.Mvc;
+
+namespace ex7.Infrastructure
+{
+    public class NinjectDependencyResolver: IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver() {
+            kernel = new StandardKernel();
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType) {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType) {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings() {
+            kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+        }
+    }
+}
