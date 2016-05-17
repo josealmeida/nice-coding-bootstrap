cce887535ffddef7b84163c88f805d8b7f3dcdbe
Jose Hilario Almeida <josealmeida@gmail.com>
2016-05-17 11:26:11 +0100

    added ProductSummary partial view and styling

--- csharp/ex9/SportsStore/SportsStore.WebUI/App_Start/RouteConfig.cs
+++ csharp/ex9/SportsStore/SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -14,21 +14,22 @@ namespace SportsStore.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: null,
+                url: "Page{page}",
+                defaults: new
+                {
+                    Controller = "Product",
+                    action = "List"
+                }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new {
                     controller = "Product",
                     action = "List",
                     id = UrlParameter.Optional
-                }
-            );
-
-            routes.MapRoute(
-                name: null, 
-                url: "Page{page}", 
-                defaults: new {
-                    Controller = "Product",
-                    action = "List"
                 }
             );
         }
