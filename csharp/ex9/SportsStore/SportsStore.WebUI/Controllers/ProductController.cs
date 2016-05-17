5146090f0714485201a72622ffd4300eb0d21cac
Jose Hilario Almeida <josealmeida@gmail.com>
2016-05-17 09:28:02 +0100

    added pagination

--- csharp/ex9/SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
+++ csharp/ex9/SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -4,19 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository productRepository) {
             this.repository = productRepository;
         }
 
-        public ViewResult List() {
-            return View(repository.Products);
+        public ViewResult List(int page = 1) {
+            return View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize));
         }
     }
 }
